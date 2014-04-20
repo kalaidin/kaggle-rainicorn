1409070438a1068430c848c5af129cce749fe202
Marat Zaynutdinov <tsundokum@gmail.com>
2014-04-20 15:33:09 +0400

    refactoring infrastructure for models -- doing in more sklearn'ish

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python3
+from sklearn.base import BaseEstimator
+from sklearn.externals.joblib import Parallel, delayed
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC, SVC
+import sys
 
 from per_customer_eval import *
 from per_customer_models import *
 
-eval_per_customer_model(LastQuoted)
-#eval_per_customer_model(SGD, n_iter=30)
 
-OneVsRestClassifier()
+class EachTaskIndependently(BaseEstimator):
+    def __init__(self, estimator, n_jobs=1):
+        self.estimator = estimator
+        self.n_jobs = n_jobs
+
+    def fit(self, X, y, sample_weight=None):
+        task_estimators = []
+        for i in range(y.shape[1]):
+            e = clone(self.estimator)
+            try:
+                e.fit(X, y[:, i], sample_weight=sample_weight)
+            except TypeError:
+                print("Seems like %s doesn't support `sample_weight` argument. Ignoring it" % str(e),
+                      file=sys.stderr)
+                e.fit(X, y[:, i])
+            p = e.predict(X)
+            yy = y[:, i]
+            print('train score %d: %0.3f' % (i, weighted_score(yy, p, sample_weight)))
+            task_estimators.append(e)
+        self.task_estimators = task_estimators
+
+    def predict(self, X):
+        return np.vstack([e.predict(X) for e in self.task_estimators]).T
+
+
+#eval_per_customer_model(LastQuoted)
+eval_per_customer_model(EachTaskIndependently(SGDClassifier(n_iter=30)))
+#eval_per_customer_model(EachTaskIndependently(SVC(kernel='linear')))
+#eval_per_customer_model(EachTaskIndependently(LinearSVC(C=0.01)))
+
+
+#OneVsRestClassifier()
--- per_customer_eval.py
+++ per_customer_eval.py
@@ -2,24 +2,28 @@
 import pickle
 import datetime
 import numpy as np
+from sklearn import clone
 from commons import *
 
 
 def weighted_score(actual, predicted, weight):
+    """
+    >>> import numpy as np
+    >>> a = np.array([[1, 1], [1, 2]])
+    >>> p = np.array([[1, 0], [1, 2]])
+    >>> w = np.array([0.7, 0.3])
+    >>> '%0.1f' % weighted_score(a, p, w)
+    '0.3'
+    """
     assert isinstance(actual, np.ndarray)
     assert isinstance(predicted, np.ndarray)
     assert isinstance(weight, np.ndarray)
 
-    corrects = 0
-    total = 0
-    for a, p, w in zip(actual, predicted, weight):
-        if tuple(a) == tuple(p):
-            corrects += w
-        total += w
-    return corrects / total
+    corrects = actual == predicted if len(actual.shape) == 1 else np.all(actual == predicted, axis=1)
+    return np.sum(corrects * weight) / sum(weight)
 
 
-def eval_per_customer_model(model_class, *params, **kwd_params):
+def eval_per_customer_model(estimator):
     corrects = 0.0
     total = 0.0
     for cv in range(CV_GROUPS_COUNT):
@@ -27,14 +31,13 @@ def eval_per_customer_model(model_class, *params, **kwd_params):
             with open(j(DATA_DIR, 'cv%02d_per-customer-train.pickle' % cv), 'rb') as f:
                 dv, train_customers, train_y, train_x, train_weights = pickle.load(f)
 
-            model = model_class(*params, feature_names=dv.get_feature_names(), **kwd_params)
-            model.fit(train_x, train_y, train_weights)
+            model = clone(estimator)
+            model.fit(train_x, train_y, sample_weight=train_weights)
 
             with open(j(DATA_DIR, 'cv%02d_per-customer-test.pickle' % cv), 'rb') as f:
                 _, test_customers, test_y, test_x, test_weights = pickle.load(f)
 
             test_p = model.predict(test_x)
-            #w = np.ones(test_y.shape[0])
             corrects += (equal_rows(test_y, test_p) * test_weights).sum()
             total += test_weights.sum()
 
@@ -61,4 +64,8 @@ def make_per_customer_submission(model_class, *params, **kwd_params):
         for c, ps in zip(test_customers, model.predict(test_x)):
             f.write('%s,%s\n' % (c, ''.join(str(pp) for pp in ps)))
 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
